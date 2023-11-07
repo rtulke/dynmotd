e0ce56adb87c3af7992c59554e418acb8270ee11
Robert Tulke <rt@debian.sh>
2023-11-07 20:57:23 +0100

    Update dynmotd.sh

--- dynmotd.sh
+++ dynmotd.sh
@@ -86,9 +86,8 @@ function createmaintenance {
 ## create .environment file if not exist
 function createenv {
 
-echo -e "
-${F2}============[ ${F1}Maintenance Information${F2} ]========================================
-"
+echo -e "${F2}============[ ${F1}Maintenance Information${F2} ]========================================${F1}"
+
         echo "We want to assign a function name for $(hostname --fqdn)"
         echo
         echo -n "System Function, like Webserver, Mailserver e.g. [${1}]: "
