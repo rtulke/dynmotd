5fe70e92c16cb27ef570e3380af4a18c715bf13e
Robert Tulke <rt@debian.sh>
2026-04-17 21:09:00 +0200

    fix

--- dynmotd.sh
+++ dynmotd.sh
@@ -992,7 +992,9 @@ ${F1}    Active Jails ${F2}= ${F3}${summary}${F1}"
                         hostname=$(host -W 1 "$ip" 2>/dev/null \
                             | awk '/domain name pointer/ {sub(/\.$/, "", $NF); print $NF; exit}')
                     fi
-                    printf "${F3}    %-12s ${F2}= ${F3}%s\n" "$ip" "${hostname:---}" > "$tmpf"
+                    local pad=$(( 14 - ${#ip} ))
+                    (( pad < 1 )) && pad=1
+                    printf "${F3}   %s%*s${F2}= ${F3}%s\n" "$ip" "$pad" "" "${hostname:---}" > "$tmpf"
                 ) &
             done
             wait
@@ -1002,7 +1004,9 @@ ${F1}    Active Jails ${F2}= ${F3}${summary}${F1}"
             done
         else
             for ip in "${ips[@]}"; do
-                printf "${F3}    %-12s ${F2}= ${F3}--\n" "$ip"
+                local pad=$(( 14 - ${#ip} ))
+                (( pad < 1 )) && pad=1
+                printf "${F3}   %s%*s${F2}= ${F3}--\n" "$ip" "$pad" ""
             done
         fi
     done
